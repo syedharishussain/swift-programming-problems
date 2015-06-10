af2caedf6474fd677509e333bdbaded571a11771
syedharishussain <syedharishussain@gmail.com>
2015-06-10 17:46:36 +0500

    added comments for pascal problem

--- PascalTriangle.playground/Contents.swift
+++ PascalTriangle.playground/Contents.swift
@@ -1,5 +1,20 @@
 //: Playground - noun: a place where people can play
 
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+        [1],
+       [1,1],
+      [1,2,1],
+     [1,3,3,1],
+    [1,4,6,4,1]
+]
+*/
+
 import UIKit
 import Foundation
 
